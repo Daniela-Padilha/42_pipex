9846c8ae43c631fc81313687de70897d22f0853c
Daniela Padilha <129836273+Daniela-Padilha@users.noreply.github.com>
2025-01-08 15:50:19 +0000

    Add files via upload

---
+++ new.c
@@ -0,0 +1,56 @@
+
+//info     --> Execute a comand
+//cmd      --> Comand to be executed
+//env      --> 
+//path     --> Path to the command
+//cmd_args --> Array of the args of the command
+
+void	exec_cmd(char *cmd, char **env)
+{
+	char	*path;
+	char	**cmd_args;
+
+	cmd_args = ft_split(cmd, ' ');
+	path = get_path();
+	if (execve(path, cmd_args, env) == -1)
+	{
+		perror("ERROR: command not found. Execution failed");
+		exit(0);
+	}
+}
+
+
+//info       --> Create child routine
+//av         --> Arg that indicates the file to open
+//pipe_fd    --> Pipe fd[2], o read e o write
+//env        --> 
+
+void	child(char **av, int *pipe_fd, char **env)
+{
+	int	fd;
+
+	fd = open_file(av[1], O);
+	dup2(fd, 0);
+	dup2(pipe_fd[1], 1);
+	close(pipe_fd[0]);
+	exec_cmd(av[2], env);
+}
+
+//info       --> Create parent routine
+//av         --> Arg that indicates the file to open
+//pipe_fd    --> Pipe fd[2], o read e o write
+//env        --> 
+
+void	parent(char **av, int *pipe_fd, char **env)
+{
+	int	fd;
+
+	fd = open_file(av[1], O);
+	dup2(fd, 1);
+	dup2(pipe_fd[0], 0);
+	close(pipe_fd[1]);
+	exec_cmd(av[3], env);
+}
+
+
+
---
+++ new_utils.c
@@ -0,0 +1,30 @@
+
+
+//info           --> Open file with specific flags depending on the process 
+//					 that we are in
+//file           --> Str with the name of the file
+//child_or_not   --> Checks if we are in the child process
+//return         --> Fd of the file openned, or -1 if error
+
+int	open_file(char *file, int child_or_not)
+{
+	int	fd;
+
+	if (child_or_not == 0)
+		fd = open(file, O_RDONLY, 0777);
+	if (child_or_not > 0)
+		fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	if (fd == -1)
+		exit(0);
+	return (fd);
+}
+
+//info       --> Get path for the cmd
+//cmd        --> Comand to be executed
+//env        --> 
+//return     --> The path
+
+char	*get_path(char *cmd, char **env)
+{
+
+}
